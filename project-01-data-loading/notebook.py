ebb93216a3f9e6286743e530c35cff7abfedc8a7
LukasAnell <lukasmanell@gmail.com>
2026-05-19 17:04:10 -0700

    Add initial project file and target dataset

---
+++ project-01-data-loading/notebook.py
@@ -0,0 +1,17 @@
+# %% Imports
+import pandas as pd
+
+# %% Load data
+df = pd.read_csv("data/2019.csv")
+
+# %% Shape
+
+
+# %% First and last rows
+
+
+# %% Column names and types
+
+
+# %% Null counts
+
